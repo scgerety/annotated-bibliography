d54659ae3f687737e56adcb3b010be09f468a112
Sean Gerety <scgerety@gmail.com>
2026-04-20 06:46:48 -0400

    Fine tuning usage of CORE API.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ version=0.0.1
 author=scgerety
 """
 
-
+import json
 import os
 import sys
 import requests
@@ -22,15 +22,15 @@ def main():
     show_all()
 
 def query(query=query, api_key=CORE_api):
-    url = "https://api.core.ac.uk/v3/search/works"   
+    url = "https://api.core.ac.uk/v3/search/works"
     headers = {"Authorization": f"Bearer {api_key}"}
-    params = {"q": {"abstract": f"{query}"}}
+    params = {"q": {"title": f"{query}"}, "scroll": True, "limit": 100}
 
     try:
         response = requests.get(url, headers=headers, params=params)
         if response.status_code == 200:
             with open(result_json, "w") as r:
-                r.write(f"{response.json()}")
+                json.dump(response.json(), r)
         else:
             print(f"Error: {response.status_code}: {response.text}")
             return None
@@ -40,8 +40,9 @@ def query(query=query, api_key=CORE_api):
 
 
 def show_all():
+
     df = pd.read_json(result_json)
-    df.to_csv(result_csv)
+
 
 if __name__ == "__main__":
     main()
