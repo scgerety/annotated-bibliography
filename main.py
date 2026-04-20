76d107f0d9361f99fc385e4f0dc6870c74f3b668
Sean Gerety <scgerety@gmail.com>
2026-04-20 16:06:56 -0400

    Added training Top2Vec model and saving tokens to doc.

--- main.py
+++ main.py
@@ -10,16 +10,19 @@ import os
 import sys
 import requests
 import pandas as pd
+from top2vec import Top2Vec
 
 CORE_api = os.getenv('CORE')
+HF_TOKEN = os.getenv('HF_TOKEN')
 query = " ".join(sys.argv[1:])
 this_dir = os.path.dirname(os.path.abspath(__file__))
 result_json = os.path.join(this_dir, "result.json")
 result_csv = os.path.join(this_dir, "result.csv")
 
 def main():
-    query()
+    # query()
     df = load_result()
+    # train_model(df)
     df = analyze_full_text(df)
     save_result(df)
 
@@ -61,15 +64,36 @@ def load_result():
     return df
 
 
+def train_model(df):
+    docs = df.fullText.tolist()
+    model = Top2Vec(docs,
+                    speed="deep-learn",
+                    workers=8,
+                    ngram_vocab=True,
+                    contextual_top2vec=True, # Hoping this will work.
+                    )
+
+    model.save("bib.model")
+
+
 def analyze_full_text(df):
-   df = df[[
+    model = Top2Vec.load("bib.model")
+    doc_tokens = [doc for doc in model.get_document_tokens()]
+
+    with open("tokens.csv", "a") as token_list:
+        for tokens in doc_tokens:
+            token_list.write(",".join(tokens))
+            token_list.write("\n")
+
+    df = df[[
         "id",
         "title",
         "authors",
         "abstract",
     ]] 
-   df["topics"] = 0
-   return df
+    df["topics"] = ""
+    return df
+
 
 def save_result(df):
     df.to_csv(result_csv)
